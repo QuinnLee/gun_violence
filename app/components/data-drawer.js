43d0f92e7ccfd31a3dffd7a1e54025bb73d471bd
Quinn Lee <leekeunhwi@gmail.com>
2016-06-29 09:49:11 -0400

    font-color update

--- app/components/data-drawer.js
+++ app/components/data-drawer.js
@@ -11,35 +11,35 @@ export default Ember.Component.extend({
   }),
   displayDateRange: computed('dateRange', function() {
     let dateRange = get(this, 'dateRange');
-    if(dateRange[0] === dateRange[1]) { return `On ${moment(dateRange[1]).format('MMMM Do YYYY')}`; }
-    return `From ${moment(dateRange[0]).format('MMMM Do YYYY')} to ${moment(dateRange[1]).format('MMMM Do YYYY')}`;
+    if(dateRange[0] === dateRange[1]) { return `<p> On <u>${moment(dateRange[1]).format('MMMM Do YYYY')}</u></p>`; }
+    return `<p> From <u>${moment(dateRange[0]).format('MMMM Do YYYY')}</u> to <u>${moment(dateRange[1]).format('MMMM Do YYYY')}</u></p>`;
   }),
   displayCount: computed('filteredData', 'data',  function() {
     let data = get(this, 'filteredData') || get(this, 'data');
     let count = data.length;
 
     if(count === 1) {
-      return 'was 1 Mass shooting';
+      return '<p> there was <u>1</u> Mass shooting</p>';
     } else {
-      return `were ${count} Mass shootings`;
+      return `<p> there were <u>${count}</u> Mass shootings</p>`;
     }
   }),
   deadCount: computed('filteredData', 'data',  function() {
     let data = get(this, 'filteredData') || get(this, 'data');
     let count = _.reduce(data, (memo, d) => memo + get(d, 'dead'), 0)
     if(count === 1) {
-      return '1 person ';
+      return '<p> <u class="dead--copy">1</u> person died </p>';
     } else {
-      return `${count} people`;
+      return `<p> <u class="dead--copy">${count}</u> people </p>`;
     }
   }),
   injuredCount: computed('filteredData', 'data',  function() {
     let data = get(this, 'filteredData') || get(this, 'data');
     let count = _.reduce(data, (memo, d) => memo + get(d, 'injured'), 0)
     if(count === 1) {
-      return '1 person ';
+      return '<p><u class="injured--copy">1</u> person got injured</p>';
     } else {
-      return `${count} people`;
+      return `<p><u class="injured--copy">${count}</u> people got injured</p>`;
     }
   })
 });
